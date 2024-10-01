f396a8ec68c140dd1c05872edad55f72728966c3
David Gilvan Souza <97628609+DGjiban@users.noreply.github.com>
2024-10-01 19:13:28 -0400

    update
    logout button and retrieve data from firebase

--- app/src/main/java/sheridancollege/capstoneproject/mindmatrix/ui/home/HomeFragment.kt
+++ app/src/main/java/sheridancollege/capstoneproject/mindmatrix/ui/home/HomeFragment.kt
@@ -1,13 +1,17 @@
 package sheridancollege.capstoneproject.mindmatrix.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import sheridancollege.capstoneproject.mindmatrix.databinding.FragmentHomeBinding
+import sheridancollege.capstoneproject.mindmatrix.ui.login.LoginActivity
 
 class HomeFragment : Fragment() {
 
@@ -17,22 +21,62 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.txtWelcome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val email = currentUser?.email
+
+        if (email != null) {
+            db.collection("users").whereEqualTo("email", email).get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val name = document.getString("name")
+                        val rank = document.getString("rank")
+
+                        // Update the textviews with the database info
+                        binding.txtWelcome.text = "Welcome $name"
+                        binding.txtRank.text = rank
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // if error
+                    Log.w("Firestore", "Error: ", exception)
+                }
         }
+
+        val logout = binding.btLogout.setOnClickListener{
+            logout()
+        }
+
         return root
+    }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut() // Firebase logout
+        goToLoginScreen() // Fuction to send the user back to the login page
+    }
+
+    private fun goToLoginScreen() {
+        val intent = Intent(requireActivity(), LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        requireActivity().finish() // Close the current activity
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser == null) {
+            goToLoginScreen() // If the user is not authenticate, return to the login screen
+        }
     }
 
     override fun onDestroyView() {
